27a4bc96117da939147903b18dfcae35ef1f474a
rohithsatheesh0924 <rohithips296@gmail.com>
2026-04-03 16:13:32 +0530

    chnages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // <--- Add this import
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Wrap App in BrowserRouter to enable clean SEO-friendly routing */}
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </StrictMode>,
 )
